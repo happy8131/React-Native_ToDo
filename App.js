9a4666b69d68d111d24a77ddf19b6187de8ba3fd
happy8131 <lion8131@naver.com>
2022-05-22 18:02:46 +0900

    [ADD] ToDos 3.3

--- App.js
+++ App.js
@@ -12,10 +12,22 @@ import { theme } from "./colors";
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChaangeText = (payload) => setText(payload);
-
+  const addToDo = () => {
+    if (text === "") {
+      return;
+    }
+    //save to do
+    const newToDos = Object.assign({}, toDos, {
+      [Date.now()]: { text, work: working },
+    });
+    setToDos(newToDos);
+    setText("");
+  };
+  console.log(toDos);
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -40,7 +52,9 @@ export default function App() {
       </View>
 
       <TextInput
+        onSubmitEditing={addToDo}
         onChangeText={onChaangeText}
+        returnKeyType="done"
         value={text}
         placeholder={working ? "Add a To Do" : "Where do you want to go?"}
         style={styles.input}
